b2249edf30db6465c3392e57c93ae5d649acf731
Anwen Williams <anwen@anwenwilliams.com>
2021-10-01 13:38:14 +0100

    Select page elements, set context initial line styles

--- assets/js/etch-a-sketch.js
+++ assets/js/etch-a-sketch.js
@@ -2,9 +2,19 @@
 
 // Select the elements on the page -- canvas (and 2D context), shake button
 
+const canvas = document.querySelector('#etch-a-sketch');
+const ctx = canvas.getContext('2d'); // ctx is common for 'context'
+const shakeButton = document.querySelector('.shake');
+
 // Setup canvas for drawing
 
+ctx.lineJoin = 'round'; // round to ensure smooth line edges, squared by default
+ctx.lineCap = 'round';
+ctx.lineWidth = 10; // default width 1px, don't have to specify the unit
+
 // Write a draw function
+
+
 
 // Write handler for the keys -- need to know whether to move point up/ down/ left/ right
 
