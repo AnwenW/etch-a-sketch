4ab052f5a8fa939446970beca98dfc4dfd6f4b7f
Anwen Williams <anwen@anwenwilliams.com>
2021-10-01 13:29:49 +0100

    Write comments for steps to be achieved

---
+++ assets/js/etch-a-sketch.js
@@ -0,0 +1,11 @@
+// console.log('it works'); // Always check the right JS file is linked up!
+
+// Select the elements on the page -- canvas (and 2D context), shake button
+
+// Setup canvas for drawing
+
+// Write a draw function
+
+// Write handler for the keys -- need to know whether to move point up/ down/ left/ right
+
+// Write clear canvas/'shake' function
