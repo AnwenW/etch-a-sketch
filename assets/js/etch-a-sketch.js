cdd636606f7efa04b9c549c4d33ecd015cf441aa
Anwen Williams <anwen@anwenwilliams.com>
2021-10-04 11:29:39 +0100

    Tidy comments

--- assets/js/etch-a-sketch.js
+++ assets/js/etch-a-sketch.js
@@ -1,27 +1,18 @@
-// console.log('it works'); // Always check the right JS file is linked up!
-
 // Select the elements on the page -- canvas (and 2D context), shake button
-
 const canvas = document.querySelector('#etch-a-sketch');
 const ctx = canvas.getContext('2d'); // ctx is common for 'context'
 const shakeButton = document.querySelector('.shake');
 const MOVE_AMOUNT = 10; // true constant (that won't be changed) use caps/underscores
-
-// Set width/ height variables 
-// const width = canvas.width;
-// const height = canvas.height;
 
 // Make a variable called width/height from the same canvas properties:
 const { width, height } = canvas;
 
 // Get random X and Y starting points on the canvas
 // These need to be let (not const) as values will be updated below
-
 let x = Math.floor(Math.random() * width);
 let y = Math.floor(Math.random() * height);
 
-// Setup canvas for drawing
-
+// Set up canvas for drawing
 ctx.lineJoin = 'round'; // round to ensure smooth line edges, squared by default
 ctx.lineCap = 'round';
 ctx.lineWidth = MOVE_AMOUNT; // default width 1px, don't have to specify the unit
@@ -32,7 +23,6 @@ ctx.lineTo(x, y);
 ctx.stroke();
 
 // Write a draw function
-
 function draw({ key }) {
 
     // start the path
@@ -63,15 +53,13 @@ function draw({ key }) {
     ctx.stroke();
 }
 
-// Write handler for the keys -- need to know whether to move point up/ down/ left/ right
-
+// Write handler for the keys -- need to know whether to move up/down/left/right
 function handleKey(e) {
     if(e.key.includes('Arrow')) { // arrow keys only
         e.preventDefault(); // prevent keys scrolling window when smaller than page, but should make sure UI always fits inside window
         draw({ key: e.key });
     }
 }
-
 // Listen for arrow keys
 window.addEventListener('keydown', handleKey);
 
